cd4499a2d7d588bea4d99fcc9ca1e94a33e2c9f3
ZFlem96 <zackzacharyfleming@gmail.com>
2017-03-07 09:07:29 -0700

    first stage

---
+++ VideoGameSurvey/index.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    pug = require('pug'),
+    path = require('path'),
+    route = require('./routes/routes.js'),
+    bodyParser = require('body-parser');
+
+
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(path.join(__dirname + '/public'))); 
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
+
+app.listen(3000);
---
+++ VideoGameSurvey/routes/routes.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/data');
+
+var mdb = mongoose.connection;
+mdb.on('error', console.error.bind(console, 'connection error:'));
+mdb.once('open', function (callback) {
+    
+});
